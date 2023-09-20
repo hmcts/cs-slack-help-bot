e601751eee3151b1e67e4c06b0d2dc3db2f233f5
Dan Lysiak <danlysiak@hotmail.com>
2023-09-20 09:52:41 +0100

    Correct reference

--- src/model/Product.js
+++ src/model/Product.js
@@ -39,7 +39,7 @@ class Product {
     }
 
     shouldReport() {
-        console.log(`product ${product.name} - reporting enabled: ${this.isReportingEnabled}`);
+        console.log(`product ${this.id} - reporting enabled: ${this.isReportingEnabled}`);
         if (!this.isReportingEnabled) {
             return false;
         }
