2163adee4bb068d8f732a86824f5c293a39ed737
Dan Lysiak <danlysiak@hotmail.com>
2024-06-07 09:09:55 +0100

    Remove JPS

--- src/service/serviceStatus.js
+++ src/service/serviceStatus.js
@@ -84,9 +84,6 @@ const products = [
         // new ServiceTemplate("idam-web-admin", env => prodOverride(env, `https://idam-web-admin.platform.hmcts.net`, `https://idam-web-admin.${env}.platform.hmcts.net`)),
         new ServiceTemplate("idam-web-public", env => prodOverride(env, `https://hmcts-access.service.gov.uk`, `https://idam-web-public.${env}.platform.hmcts.net`)),
     ], false),
-    new Product("jps", "Judicial Payments Service", [
-        // To be deployed
-    ], false),
     new Product("lau", "Log & Audit", [
         new ServiceTemplate("lau-case-backend", env => `http://lau-case-backend-${env}.service.core-compute-${env}.internal`),
         new ServiceTemplate("lau-frontend", env => prodOverride(env, `https://log-and-audit.platform.hmcts.net`, `https://lau.${env}.platform.hmcts.net`)),
