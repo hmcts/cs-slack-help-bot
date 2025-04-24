1add3f1398c86ec0e0b400bdcd1697806c146c38
Dan Lysiak <danlysiak@hotmail.com>
2025-04-24 22:21:50 +0100

    Use fail

--- src/workflow/getServiceStatusStep.js
+++ src/workflow/getServiceStatusStep.js
@@ -3,53 +3,53 @@ const {getAllProducts} = require("../service/serviceStatus");
 const Environment = require('../model/Environment');
 
 const getServiceStatusWorkflowStep = async ({ client, inputs, fail }) => {
-    console.log('executed step with inputs', JSON.stringify(inputs));
-    const blocks = [];
-    const products = getAllProducts();
-    
-    const env = inputs.env.value.value;
-
-    blocks.push({
-        "type": "header",
-        "text": {
-            "type": "plain_text",
-            "text": "Common Services " + env.toUpperCase(),
-        }
-    });
-
-    products.forEach(product => {
-        if (product.services[env].length > 0) {
-            blocks.push({
-                "type": "header",
-                "text": {
-                    "type": "plain_text",
-                    "text": product.desc
-                }
-            });
-            blocks.push({
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": product.getMarkdown(env)
-                }
-            });
-        }
-    });
-
-    blocks.push({ "type": "divider" });
-    
-    blocks.push({
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": `\n>This is up to date as of ${new Date().toLocaleString()} UTC \n`
-        }
-    });
-
-    console.log(JSON.stringify(blocks));
-    const channel = inputs.channel.value;
-    console.log(`publishing to channel ${channel}`);
     try {
+        console.log('executed step with inputs', JSON.stringify(inputs));
+        const blocks = [];
+        const products = getAllProducts();
+        
+        const env = inputs.env.value.value;
+
+        blocks.push({
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": "Common Services " + env.toUpperCase(),
+            }
+        });
+
+        products.forEach(product => {
+            if (product.services[env].length > 0) {
+                blocks.push({
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": product.desc
+                    }
+                });
+                blocks.push({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": product.getMarkdown(env)
+                    }
+                });
+            }
+        });
+
+        blocks.push({ "type": "divider" });
+        
+        blocks.push({
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": `\n>This is up to date as of ${new Date().toLocaleString()} UTC \n`
+            }
+        });
+
+        console.log(JSON.stringify(blocks));
+        const channel = inputs.channel.value;
+        console.log(`publishing to channel ${channel}`);
         await client.chat.postEphemeral({
             channel: channel,
             user: inputs.user.value,
@@ -59,6 +59,7 @@ const getServiceStatusWorkflowStep = async ({ client, inputs, fail }) => {
         });
     } catch (error) {
         console.error(error);
+        fail({ error: `Failed to handle function request: ${error}` });
     }
 };
 
